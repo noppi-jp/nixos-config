da8e46ed51a071aa69b8a0fc555e425d80c2b8fc
Noppi <noppi@noppi.jp>
2025-07-06 20:00:38 +0900

    fcitx5をWayland用に設定

--- configuration.nix
+++ configuration.nix
@@ -73,11 +73,15 @@
     inputMethod = {
       enable = true;
       type = "fcitx5";
-      fcitx5.addons = with pkgs; [
-        fcitx5-skk
-        fcitx5-gtk
-        libsForQt5.fcitx5-qt
-      ];
+
+      fcitx5 = {
+        waylandFrontend = true;
+        addons = with pkgs; [
+          fcitx5-skk
+          fcitx5-gtk
+          libsForQt5.fcitx5-qt
+        ];
+      };
     };
   };
 
