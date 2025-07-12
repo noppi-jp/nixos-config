f0c20f9eaaf0e2836de346e25af776703b5ca80a
Noppi <noppi@noppi.jp>
2025-07-13 00:08:21 +0900

    podman → Docker に変更

--- configuration.nix
+++ configuration.nix
@@ -34,6 +34,8 @@
     # $ nix search wget
     systemPackages = with pkgs; [
       bitwarden-desktop
+      docker-buildx
+      docker-compose
       emacs
       fastfetch
       file
@@ -41,7 +43,7 @@
       hyprshot
       p7zip
       ripgrep
-      vscode.fhs
+      vscode
       wget
       wl-clipboard
       wezterm
@@ -206,7 +208,7 @@
     # Define a user account. Don't forget to set a password with ‘passwd’.
     users.satoshi = {
       group = "satoshi";
-      extraGroups = [ "wheel" ];
+      extraGroups = [ "wheel" "docker" ];
       isNormalUser = true;
       hashedPassword = "$y$j9T$i8FidX3eyTOwJ8sRB9Zlv/$AUbG0y.3CcB2Mdj3zd3DQSo/Q7WnqG3y84MJeAqG0x4";
       openssh.authorizedKeys.keys = [
@@ -222,9 +224,6 @@
 
   virtualisation = {
     containers.enable = true;
-    podman = {
-      enable = true;
-      defaultNetwork.settings.dns_enabled = true;
-    };
+    docker.enable = true;
   };
 }
