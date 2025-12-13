88fa10a0364d8dd2cc053294eb374bc507cfb324
Noppi <noppi@noppi.jp>
2025-12-14 04:39:07 +0900

    stumptray 追加

--- home/.stumpwm.d/init.lisp
+++ home/.stumpwm.d/init.lisp
@@ -1,4 +1,7 @@
+(load-module :stumptray)
+
 (set-font "-*-unifont-medium-r-normal-*-*-*-*-*-*-*-*-*")
-(run-shell-command "xrandr --output Virtual-1 --mode 1920x1080")
-(run-shell-command "fcitx5 &")
+;; (run-shell-command "xrandr --output Virtual-1 --mode 1920x1080")
+;; (run-shell-command "fcitx5 &")
 (mode-line)
+(stumptray::stumptray)
