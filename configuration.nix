68f7e4b96f3fe42ae8db9f2f6f77500ec30c0314
Noppi <noppi@noppi.jp>
2025-12-15 08:51:32 +0900

    ガベコレ・自動最適化を有効にした

--- configuration.nix
+++ configuration.nix
@@ -103,7 +103,25 @@
     firewall.enable = false;
   };
 
-  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  nix = {
+    # Perform garbage collection weekly to maintain low disk usage
+    gc = {
+      automatic = true;
+      dates = "weekly";
+      options = "--delete-older-than 7d";
+    };
+
+    settings = {
+      # Optimize storage
+      # You can also manually optimize the store via:
+      #    nix-store --optimise
+      # Refer to the following link for more details:
+      # https://nixos.org/manual/nix/stable/command-ref/conf-file.html#conf-auto-optimise-store
+      auto-optimise-store = true;
+
+      experimental-features = [ "nix-command" "flakes" ];
+    };
+  };
 
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
